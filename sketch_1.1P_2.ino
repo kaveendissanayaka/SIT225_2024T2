9f6fdb2c96acceb8608182c477b99e0eea6794bd
Kaveen Dissanayaka <153261675+kaveendissanayaka@users.noreply.github.com>
2024-09-12 17:48:57 +1000

    Add files via upload

---
+++ sketch_1.1P_2.ino
@@ -0,0 +1,25 @@
+
+
+int x;
+
+void setup() {
+  Serial.begin(9600);  // set baud rate
+}
+
+void loop() {
+  while (!Serial.available()) {} // wait for data to arrive
+
+  // read string data from Serial, we know Python 
+  // script is sending just an integer.
+  x = Serial.readString().toInt();
+
+  // write a string (no newline)
+  Serial.print("Arduino sends: ");  
+
+  // Add 1 to what received.
+  // Write an integer with a newline (println vs print).
+  Serial.println(x + 1);  
+
+  // Push the data through serial channel.
+  Serial.flush();  
+}
